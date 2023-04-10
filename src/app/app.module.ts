b47ab153d6b93873679c84c1947d33d7488482cc
susantab86 <130149121+susantab86@users.noreply.github.com>
2023-04-10 15:35:48 +0530

    4 files modified, 1 added

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  imports: [CommonModule, BrowserModule],
+  declarations: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
